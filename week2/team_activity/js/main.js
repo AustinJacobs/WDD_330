59b6673d3a95d5452a3e0227c9a08ed607891a24
AustinJacobs <aust.t.jac@gmail.com>
2021-09-22 16:56:22 -0600

    finsihed week2 Team Activity

--- week2/team_activity/js/main.js
+++ week2/team_activity/js/main.js
@@ -1,17 +1,24 @@
 function getInput() {
     let text = document.getElementById('user').value;
-
     document.getElementById('output').innerHTML = text;
 }
 
 function addNumber() {
-    let number = parseInt(document.getElementById('number').value);
-    
+    let number = parseInt(document.getElementById('integer').value);
+
     let sum = number * (number + 1) / 2;
 
-    document.getElementById('outputTwo').innerHTML = sum;
+    if (number !== NaN) {
+        document.getElementById('outputTwo').innerHTML = sum;
+    }
 }
 
 function addingMachine() {
-    
+    let one = parseInt(document.getElementById("inputOne").value);
+    let two = parseInt(document.getElementById("inputTwo").value);
+
+    if ((one !== NaN) & (two !== NaN)) {
+        let add = one + two;
+        document.getElementById('outputThree').innerHTML = add;
+    }
 }

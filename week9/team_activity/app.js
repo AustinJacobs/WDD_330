414c10d9bb4fb20d00abb90929894ed469dc4132
AustinJacobs <aust.t.jac@gmail.com>
2021-11-12 00:23:07 -0700

    finished team challenge

--- week9/team_activity/app.js
+++ week9/team_activity/app.js
@@ -1,50 +1,28 @@
-// document.addEventListener("keydown", function (e) {
-//     if (e.keyCode == '65') {
-//         let myAudio = new Audio("sounds/boom.wav");
-//         myAudio.play();
-//         let a = 
-//     } 
-//     else if (e.keyCode == '83') {
-//         let myAudio = new Audio("sounds/hihat.wav");
-//         myAudio.play();
-//     }
-//     else if (e.keyCode == '68') {
-//         let myAudio = new Audio("sounds/kick.wav");
-//         myAudio.play();
-//     }
-//     else if (e.keyCode == '70') {
-//         let myAudio = new Audio("sounds/openhat.wav");
-//         myAudio.play();
-//     }
-//     else if (e.keyCode == '71') {
-//         let myAudio = new Audio("sounds/boom.wav");
-//         myAudio.play();
-//     }
-//     else if (e.keyCode == '72') {
-//         let myAudio = new Audio("sounds/ride.wav");
-//         myAudio.play();
-//     }
-//     else if (e.keyCode == '74') {
-//         let myAudio = new Audio("sounds/snare.wav");
-//         myAudio.play();
-//     }
-//     else if (e.keyCode == '75') {
-//         let myAudio = new Audio("sounds/tom.wav");
-//         myAudio.play();
-//     }
-//     else if (e.keyCode == '76') {
-//         let myAudio = new Audio("sounds/tink.wav");
-//         myAudio.play();
-//     }
-// });
-
 document.addEventListener("keydown", function (e) {
     const audios = document.querySelectorAll("audio");
+
     for (let number = 0; number < audios.length; number++) {
-        console.log(audios[number].dataset.key);
+
         if (e.keyCode == audios[number].dataset.key) {
-            var myAudio = new Audio(audios[number].src);
+            let myAudio = new Audio(audios[number].src);
             myAudio.play();
+        }
+    }
+
+    const keys = document.querySelectorAll(".key");
+
+    for (let i = 0; i < keys.length; i++) {
+
+        if (e.keyCode == keys[i].dataset.key) {
+            keys[i].classList.add("playing");
+
+            keys[i].classList.add("drop");
+
+            setTimeout(timeFunction, 100);
+        }
+
+        function timeFunction() {
+            keys[i].classList.remove("playing");
         }
     }
 });
